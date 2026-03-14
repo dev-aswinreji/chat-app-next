d6795645b9cf8b34eabd3bf7a0314dd2d0a7c0af
Aswin Reji <aswinreji.004@gmail.com>
2026-03-14 05:48:34 +0000

    fix: ensure jwt strategy registered

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
 import { AuthModule } from './auth/auth.module';
 import { ChatModule } from './chat/chat.module';
 import { UsersModule } from './users/users.module';
 import { MessagesModule } from './messages/messages.module';
+import { JwtStrategy } from './auth/strategies/jwt.strategy';
 
 @Module({
-  imports: [AuthModule, ChatModule, UsersModule, MessagesModule],
+  imports: [PassportModule.register({ defaultStrategy: 'jwt' }), AuthModule, ChatModule, UsersModule, MessagesModule],
+  providers: [JwtStrategy],
 })
 export class AppModule {}
